72c5980f998eca3d6f86d9714bf969106a951a4b
Juraj Paluba <juraj.palubajp@gmail.com>
2025-05-08 20:45:38 +0200

    feat(fedora-init): config awesome session
    So I can choose awesome while logging in to system.

--- fedora-init/awesome.sh
+++ fedora-init/awesome.sh
@@ -8,3 +8,20 @@ echo "exec awesome" > ~/.xinitrc
 echo "copy awesome config"
 cp -r $REPO_ROOT/awesome ~/.config/
 mv ~/.config/awesome/rc.fedora.lua ~/.config/awesome/rc.lua
+
+echo "configure awesome as one of gnome sessions"
+echo "[GNOME Session]
+Name=Awesome session
+RequiredComponents=awesome;gnome-settings-daemon;
+DesktopName=Awesome
+" | sudo tee /usr/share/gnome-session/sessions/awesome.session
+
+curl https://raw.githubusercontent.com/awesomeWM/awesome/master/awesome.desktop | sudo tee /usr/share/xsessions/awesome.desktop
+# echo "[Desktop Entry]
+# Version=1.0
+# Type=Application
+# Name=Awesome
+# Comment=The awesome launcher!
+# TryExec=/usr/bin/awesome
+# Exec=/usr/bin/awesome
+# " | sudo tee /usr/share/applications/awesome.desktop
